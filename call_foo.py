c3949aae68435f5bdcc9289323026686db0c01ad
William Kong <wwkong92@gmail.com>
2021-08-28 23:36:46 -0400

    More bug fixes.

--- call_foo.py
+++ call_foo.py
@@ -2,17 +2,29 @@ from ctypes import c_char_p, c_void_p
 import ctypes, os
 libfoo = ctypes.CDLL(os.getcwd() + '/foo.so')
 libfoo.Foo_get.restype = c_char_p
-libfoo.Foo_new.argtype = [c_char_p]
-libfoo.Foo_set.argtype = [c_void_p, c_char_p]
+
+# # GOOD CODE
+# libfoo.Foo_new.argtype = [c_char_p]
+# libfoo.Foo_set.argtype = [c_void_p, c_char_p]
 
 class Foo:
     def __init__(self, istr):
-        istr = bytes(istr, 'utf-8')
-        self.foo = libfoo.Foo_new(c_char_p(istr))
+
+        # # GOOD CODE
+        # istr = bytes(istr, 'utf-8')
+        # self.foo = libfoo.Foo_new(c_char_p(istr))
+
+        # BAD CODE
+        self.foo = libfoo.Foo_new(istr)
 
     def set_desc(self, new_str):
-        new_str = bytes(new_str, 'utf-8')
-        libfoo.Foo_set(self.foo, c_char_p(new_str))
+
+        # # GOOD CODE
+        # new_str = bytes(new_str, 'utf-8')
+        # libfoo.Foo_set(self.foo, c_char_p(new_str))
+
+        # BAD CODE
+        libfoo.Foo_set(self.foo, new_str)
 
     def get_desc(self):
         return ("Printing the contents of Foo from Python: " + str(libfoo.Foo_get(self.foo)))
@@ -28,7 +40,7 @@ x.print_ref()
 x.set_desc("Here is a new string")
 print(x.get_desc())
 x.print_desc()
-y = Foo("blah" * 2)  # Add/remove this line to toggle this new problem
+y = Foo("blah")  # Add/remove this line to toggle this new problem
 y.print_ref()
 print(x.get_desc())
 x.print_desc()
--- foo.cpp
+++ foo.cpp
@@ -1,20 +1,27 @@
 #include <vector>
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 
 extern "C" {
     class Foo {
       public:
         char* desc;
-        Foo(char* istr): desc(istr) {
+        Foo(char* istr) {
+            size_t n = strlen(istr);
+            desc = (char*) malloc(n * sizeof(char));
+            strncpy(desc, istr, n * sizeof(char));
             printf("Initializing Foo with string: %s\n", istr);
         }
     };
     Foo* Foo_new(char* bar) {
         return new Foo(bar);
     }
-    void Foo_set(Foo *foo, char* new_bar) {
-        foo->desc = new_bar;
+    void Foo_set(Foo *foo, char* new_str) {
+        size_t n = strlen(new_str);
+        foo->desc = (char*) malloc(n * sizeof(char));
+        strncpy(foo->desc, new_str, n * sizeof(char));
+        printf("Updating Foo with new string: %s\n", new_str);
     }
     char* Foo_get(Foo *foo) {
         return foo->desc;
