54a9efc0dfea401a50fcdf2495303d37fd4f4439
William Kong <wwkong92@gmail.com>
2021-08-28 22:55:43 -0400

    Bug fixes and debug statements.

--- call_foo.py
+++ call_foo.py
@@ -1,22 +1,34 @@
+from ctypes import c_char_p, c_void_p
 import ctypes, os
 libfoo = ctypes.CDLL(os.getcwd() + '/foo.so')
-libfoo.Foo_get.restype = ctypes.c_char_p
-libfoo.Foo_new.argtype = ctypes.c_char_p
-libfoo.Foo_set.argtype = ctypes.c_void_p, ctypes.c_char_p
+libfoo.Foo_get.restype = c_char_p
+libfoo.Foo_new.argtype = [c_char_p]
+libfoo.Foo_set.argtype = [c_void_p, c_char_p]
 
 class Foo:
     def __init__(self, istr):
-        # self.foo = libfoo.Foo_new(istr.encode('utf-8'))
-        self.foo = libfoo.Foo_new(istr)
+        istr = bytes(istr, 'utf-8')
+        self.foo = libfoo.Foo_new(c_char_p(istr))
 
     def set_desc(self, new_str):
-        # libfoo.Foo_set(self.foo, new_str.encode('utf-8'))
-        libfoo.Foo_set(self.foo, new_str)
+        new_str = bytes(new_str, 'utf-8')
+        libfoo.Foo_set(self.foo, c_char_p(new_str))
 
     def get_desc(self):
-        return libfoo.Foo_get(self.foo)
+        return ("Printing the contents of Foo from Python: " + str(libfoo.Foo_get(self.foo)))
+
+    def print_desc(self):
+        libfoo.Foo_print(self.foo)
+
+    def print_ref(self):
+        libfoo.Foo_ref(self.foo)
 
 x = Foo("Here is a random string")
+x.print_ref()
 x.set_desc("Here is a new string")
 print(x.get_desc())
-
+x.print_desc()
+y = Foo("blah" * 2)  # Add/remove this line to toggle this new problem
+y.print_ref()
+print(x.get_desc())
+x.print_desc()
--- foo.cpp
+++ foo.cpp
@@ -1,10 +1,14 @@
 #include <vector>
+#include <iostream>
+#include <stdio.h>
 
 extern "C" {
     class Foo {
       public:
         char* desc;
-        Foo(char* istr): desc(istr) {}
+        Foo(char* istr): desc(istr) {
+            printf("Initializing Foo with string: %s\n", istr);
+        }
     };
     Foo* Foo_new(char* bar) {
         return new Foo(bar);
@@ -15,4 +19,11 @@ extern "C" {
     char* Foo_get(Foo *foo) {
         return foo->desc;
     }
+    void Foo_print(Foo *foo) {
+        printf("Printing the contents of Foo from C++: %s\n", foo->desc);
+    }
+    void Foo_ref(Foo *foo) {
+        std::cout << "The address of this object is: " << foo << std::endl;
+    }
+
 }
